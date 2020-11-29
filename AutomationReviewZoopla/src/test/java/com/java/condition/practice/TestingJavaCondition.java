02b83776c07d2b277ced57ce9494b17d78d3657f
studentqa2020 <sarow@LAPTOP-5EBHP9EP.myfiosgateway.com>
2020-11-29 12:57:34 -0500

    Added cucumber

--- AutomationReviewZoopla/src/test/java/com/java/condition/practice/TestingJavaCondition.java
+++ AutomationReviewZoopla/src/test/java/com/java/condition/practice/TestingJavaCondition.java
@@ -1,31 +1,54 @@
 package com.java.condition.practice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class TestingJavaCondition {
+	//String browser="chrome";
+	@Test
+	@Parameters(value="browser")
 	
-	public void getsalary(){
-		
-		int age = 45;
-		
-		if(age <21) {//false
+	public void getsalary(String browser){
+
+		if(browser.contains("chrome")) {//false
+			System.out.println(" found my chrome browser");
 			
-			System.out.println("Not Adult");
-		}else if(age>65) {//false
-			System.out.println("Senior citizen");
-		}else {//here
-			System.out.println("Age not in my range");
+			System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			//FB
+			//driver.get("https://www.facebook.com/");
+			driver.navigate().to("https://www.facebook.com/");
+			
+		}else if(browser.contains("Safari")) {//false
+			System.out.println(" found my safari browser");
+			
+			System.setProperty("webdriver.safari.driver", "Path");
+			WebDriver driver = new SafariDriver();
+			driver.manage().window().maximize();
+			//FB
+			//driver.get("https://www.facebook.com/");
+			driver.navigate().to("https://www.facebook.com/");
+		}else if(browser.contains("ie")) {//true
+			System.out.println(" found my ie browser");
+			
+			System.setProperty("webdriver.ie.driver", "./Driver/IEDriverServer.exe");
+			WebDriver driver = new InternetExplorerDriver();
+			driver.manage().window().maximize();
+			//FB
+			//driver.get("https://www.facebook.com/");
+			driver.navigate().to("https://www.google.com/");
+		}else {//run
+			System.out.println("Browser not found ");
 		}
 		
-	
-		
-		
 	}
 	
-	public static void main(String[] args) {
-		//How to call= create object ==> RT
-		//Why need to create object? ==> confused (No static word)
-		
-		TestingJavaCondition obj = new TestingJavaCondition();
-		obj.getsalary();
-	}
+	
+
 
 }
--- AutomationReviewZoopla/src/test/java/com/java/looping/JavaLoop.java
+++ AutomationReviewZoopla/src/test/java/com/java/looping/JavaLoop.java
@@ -1,32 +1,25 @@
 package com.java.looping;
 
 public class JavaLoop {
-	//0 to 1000
+
+	// 0 to 1000
+	
 	public static void main(String[] args) {
-
-		//for(startpoint;end point;++/--) {
-			
-		//}
-//		for(int i=99;i<=1000;i++) {
-//			System.out.println(i);
+//		//(start point;end point;++)
+//		for(int i=0;;i++) {
+//		System.out.println(i);	
+//		if(i==888) {
+//			System.out.println("Found 888 and now breaking the loop");
+//			break;
 //		}
-		//for(startpoint;???;++/--) {
-		
-				//}
-//				for(int i=99;;i++) {
-//					System.out.println(i);
-//					if(i==200) {
-//						break;
-//					}
-//				}
-		
-		//how to create an infinite loop in java?
-		//1) for loop with no end point, 2) while loop
-		
-		while(2<4) {//true
-			System.out.println("Have a nice day");
-			break;
-		}
-	}
+//		
+//		}
 
+	while(9>3) {//condition must true
+		//code
+		System.out.println("Good night");
+		break;
+	}
+	
+	}	
 }
